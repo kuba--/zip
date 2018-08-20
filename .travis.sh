f44bd867c7c80a6d390a48b87187c7655d51cbf8
kuba-- <kuba@sourced.tech>
2018-08-21 00:37:04 +0200

    build in current dir

--- .travis.sh
+++ .travis.sh
@@ -3,12 +3,9 @@
 # Build script for travis-ci.org builds.
 #
 
-mkdir build
-cd build
-
 if [ $ANALYZE = "true" ] && [ "$CC" = "clang" ]; then
     # scan-build -h
-    scan-build cmake -G "Unix Makefiles" ..
+    scan-build cmake -G "Unix Makefiles"
     scan-build -enable-checker security.FloatLoopCounter \
         -enable-checker security.insecureAPI.UncheckedReturn \
         --status-bugs -v \
@@ -17,7 +14,7 @@ if [ $ANALYZE = "true" ] && [ "$CC" = "clang" ]; then
     ctest -VV
     find . -name '*.gcno' -exec "gcov" {} \;
 else
-    cmake -DCMAKE_BUILD_TYPE=Debug -DSANITIZE_ADDRESS=On ..
+    cmake -DCMAKE_BUILD_TYPE=Debug -DSANITIZE_ADDRESS=On
     make
     ASAN_OPTIONS=detect_leaks=0 LSAN_OPTIONS=verbosity=1:log_threads=1 ctest -V
 fi
