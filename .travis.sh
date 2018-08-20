9760143c75891bf33f68d135c6689ba68fbc2f1f
kuba-- <kuba@sourced.tech>
2018-08-21 00:26:46 +0200

    fix

--- .travis.sh
+++ .travis.sh
@@ -16,7 +16,7 @@ if [ $ANALYZE = "true" ] && [ "$CC" = "clang" ]; then
 
     ctest -VV
     find . -name '*.gcno' -exec "gcov" {} \;
-    bash <(curl -s https://codecov.io/bash)
+    curl -s https://codecov.io/bash
 else
     cmake -DCMAKE_BUILD_TYPE=Debug -DSANITIZE_ADDRESS=On ..
     make
