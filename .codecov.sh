628be8d769ac6c2df46a28aaede22e915614591b
kuba-- <kuba@sourced.tech>
2018-08-21 01:41:26 +0200

    fix

--- .codecov.sh
+++ .codecov.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+function codecov() {
 local dir
-dir=$(git rev-parse --show-toplevel)
-cd "$dir" || exit 1
+    local dir=$(git rev-parse --show-toplevel)
+    cd "$dir" || exit 1
 
-local gcov
-case "$CC" in
-    gcc*) gcov=${CC/gcc/gcov}
-        ;;
-    *) gcov="llvm-cov gcov"
-        ;;
-esac
-find build -name '*.gcno' -exec "$gcov" {} \;
-bash <(curl -s https://codecov.io/bash) || \
-    echo "Codecov did not collect coverage reports"
+    local gcov
+    case "$CC" in
+        gcc*) gcov=${CC/gcc/gcov}
+            ;;
+        *) gcov="llvm-cov gcov"
+            ;;
+    esac
+    find build -name '*.gcno' -exec "$gcov" {} \;
+    bash <(curl -s https://codecov.io/bash) || \
+        echo "Codecov did not collect coverage reports"
+}
+
+codecov()
