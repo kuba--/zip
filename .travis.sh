f3059bdb219ee32c6625bbec015bdb9c319db145
kuba-- <kuba@sourced.tech>
2018-08-21 01:55:09 +0200

    new approach

--- .travis.sh
+++ .travis.sh
@@ -2,19 +2,16 @@
 #
 # Build script for travis-ci.org builds.
 #
-mkdir build
-cd build
-
 if [ $ANALYZE = "true" ] && [ "$CC" = "clang" ]; then
     # scan-build -h
-    scan-build cmake -G "Unix Makefiles" ..
+    scan-build cmake -G "Unix Makefiles"
     scan-build -enable-checker security.FloatLoopCounter \
         -enable-checker security.insecureAPI.UncheckedReturn \
         --status-bugs -v \
         make -j 8 \
         make -j 8 test
 else
-    cmake -DCMAKE_BUILD_TYPE=Debug -DSANITIZE_ADDRESS=On ..
+    cmake -DCMAKE_BUILD_TYPE=Debug -DSANITIZE_ADDRESS=On
     make -j 8
     ASAN_OPTIONS=detect_leaks=0 LSAN_OPTIONS=verbosity=1:log_threads=1 ctest -V
 fi
