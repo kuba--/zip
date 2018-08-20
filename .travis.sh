1433689102859a9945a77cd6f518e5ab883ce689
kuba-- <kuba@sourced.tech>
2018-08-21 01:20:12 +0200

    debug

--- .travis.sh
+++ .travis.sh
@@ -12,15 +12,15 @@ if [ $ANALYZE = "true" ] && [ "$CC" = "clang" ]; then
     scan-build -enable-checker security.FloatLoopCounter \
         -enable-checker security.insecureAPI.UncheckedReturn \
         --status-bugs -v \
-        make -j 8
+        make -j 8 \
         make -j 8 test
 else
     cmake -DCMAKE_BUILD_TYPE=Debug -DSANITIZE_ADDRESS=On ..
-    make
+    make -j 8
     ASAN_OPTIONS=detect_leaks=0 LSAN_OPTIONS=verbosity=1:log_threads=1 ctest -V
 fi
 
 # upload code coverage
-ls -la
+ls -la ./test/CMakeFiles/test.exe.dir/__/src/
 find . -name "*.gcno" -exec "gcov" '{}' \;
 bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
