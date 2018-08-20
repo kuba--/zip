a58c774c229c58f5886a02035efa511eeb4bfb92
kuba-- <kuba@sourced.tech>
2018-08-21 01:00:03 +0200

    codecov

--- .travis.sh
+++ .travis.sh
@@ -2,22 +2,25 @@
 #
 # Build script for travis-ci.org builds.
 #
+set -e
+mkdir build
+cd build
 
 if [ $ANALYZE = "true" ] && [ "$CC" = "clang" ]; then
     # scan-build -h
-    scan-build cmake -G "Unix Makefiles"
+    scan-build cmake -G "Unix Makefiles" ..
     scan-build -enable-checker security.FloatLoopCounter \
         -enable-checker security.insecureAPI.UncheckedReturn \
         --status-bugs -v \
         make -j 8
-
-    make test
-    tree -sha .
-    find . -name '*.gcno' -exec "gcov" {} \; -print
-    bash <(curl -s https://codecov.io/bash) || \
-        echo "Codecov did not collect coverage reports"
 else
-    cmake -DCMAKE_BUILD_TYPE=Debug -DSANITIZE_ADDRESS=On
+    cmake -DCMAKE_BUILD_TYPE=Debug -DSANITIZE_ADDRESS=On ..
     make
     ASAN_OPTIONS=detect_leaks=0 LSAN_OPTIONS=verbosity=1:log_threads=1 ctest -V
 fi
+
+# upload code coverage
+cmake -DCMAKE_BUILD_TYPE=Debug ..
+make -j 8 test
+find . -name "*.gcno" -exec "gcov" '{}' \;
+bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
