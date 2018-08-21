3f474370ca793777b152c30efbc603df4f1da8f5
Kuba Podgórski <kuba--@users.noreply.github.com>
2018-08-21 02:40:18 +0200

    Codecov (#70)

--- .travis.sh
+++ .travis.sh
@@ -1,23 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Build script for travis-ci.org builds.
 #
-
-mkdir build
-cd build
-
 if [ $ANALYZE = "true" ] && [ "$CC" = "clang" ]; then
     # scan-build -h
-    scan-build cmake -G "Unix Makefiles" ..
+    scan-build cmake -G "Unix Makefiles"
     scan-build -enable-checker security.FloatLoopCounter \
         -enable-checker security.insecureAPI.UncheckedReturn \
         --status-bugs -v \
-        make -j 8
-
-    ctest -VV
-    find . -name '*.gcno' -exec "gcov" {} \;
+        make -j 8 \
+        make -j 8 test
 else
-    cmake -DCMAKE_BUILD_TYPE=Debug -DSANITIZE_ADDRESS=On ..
-    make
+    cmake -DCMAKE_BUILD_TYPE=Debug -DSANITIZE_ADDRESS=On
+    make -j 8
     ASAN_OPTIONS=detect_leaks=0 LSAN_OPTIONS=verbosity=1:log_threads=1 ctest -V
 fi
