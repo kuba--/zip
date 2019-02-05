1fcb1718eed35c64c93743d1a3dc26f6546b9ab2
kuba-- <kuba@sourced.tech>
2019-02-05 15:30:55 +0100

    Support ssize_t for GCC on RPI3

--- src/zip.h
+++ src/zip.h
@@ -20,7 +20,7 @@ extern "C" {
 #endif
 
 #if !defined(_SSIZE_T_DEFINED) && !defined(_SSIZE_T_DEFINED_) &&               \
-    !defined(_SSIZE_T) && !defined(_SSIZE_T_)
+    !defined(_SSIZE_T) && !defined(_SSIZE_T_) && !defined(__ssize_t_defined)
 #define _SSIZE_T
 typedef long  ssize_t;  /* byte count or error */
 #endif
