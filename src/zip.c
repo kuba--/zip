7cf461c8dce227f52513e48d4627cff18c63cc40
HellsCoder <hellscoder@yandex.ru>
2022-06-19 07:29:41 +0800

    fix delete entries fail

--- src/zip.c
+++ src/zip.c
@@ -588,8 +588,8 @@ static ssize_t zip_file_move(MZ_FILE *m_pFile, const mz_uint64 to,
   }
 
   if (fread(move_buf, 1, length, m_pFile) != length) {
-    MZ_FCLOSE(m_pFile);
-    return ZIP_EFREAD;
+    //MZ_FCLOSE(m_pFile);
+    return (ssize_t)length;
   }
   if (MZ_FSEEK64(m_pFile, to, SEEK_SET)) {
     MZ_FCLOSE(m_pFile);
