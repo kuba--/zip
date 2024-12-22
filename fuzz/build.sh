74e734ae951fe9e7cc27c6c6c7a9202b600ebba6
bcapuano <bcapuano@asu.edu>
2024-12-22 16:23:16 -0700

    Initial commit - two fuzzing harnesses and build script

---
+++ fuzz/build.sh
@@ -0,0 +1,8 @@
+cd $SRC/zip
+
+mkdir -p build
+cmake -S . -B build -DCMAKE_C_COMPILER_WORKS=1 -DZIP_BUILD_FUZZ=ON && cmake --build build --target install
+
+# Prepare corpora
+zip -q $OUT/read_entry_fuzzer_seed_corpus.zip corpus/*
+zip -q $OUT/create_zip_fuzzer_seed_corpus.zip corpus/*
