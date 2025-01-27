1bd78e4f34da4541f4bbaed3d76f8aaec11d6599
MCLoebl <matthias.loebl@rwth-aachen.de>
2025-01-27 20:08:16 +0100

    correct same name

--- fuzz/create_zip_file_fuzzer.c
+++ fuzz/create_zip_file_fuzzer.c
@@ -11,9 +11,17 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, const size_t size)
     fwrite(data, size, 1, file);
     fclose(file);
 
-    char* temp_zip_name = tmpnam(NULL);
+    int n = strlen(temp_name);
+    char* temp_zip_name = malloc(n + 5);
+    memcpy(temp_zip_name, temp_name, n);
+    temp_zip_name[n] = '.';
+    temp_zip_name[n + 1] = 'z';
+    temp_zip_name[n + 2] = 'i';
+    temp_zip_name[n + 3] = 'p';
+    temp_zip_name[n + 4] = '\0';
     const char *filenames[] = {temp_name};
     zip_create(temp_zip_name, filenames, 1);
+    free(temp_zip_name);
 
     unlink(temp_name);
     unlink(temp_zip_name);
