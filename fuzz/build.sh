0583102fc47ce56e705f3189421c4c0d03f00580
MCLoebl <matthias.loebl@rwth-aachen.de>
2025-02-06 08:57:26 +0100

    rename fuzzers

--- fuzz/build.sh
+++ fuzz/build.sh
@@ -4,5 +4,5 @@ mkdir -p build
 cmake -S . -B build -DCMAKE_C_COMPILER_WORKS=1 -DZIP_BUILD_FUZZ=ON && cmake --build build --target install
 
 # Prepare corpora
-zip -q $OUT/read_entry_fuzzer_seed_corpus.zip fuzz/corpus/*
-cp $OUT/read_entry_fuzzer_seed_corpus.zip $OUT/create_zip_fuzzer_seed_corpus.zip
+zip -q $OUT/zip_entry_read_fuzzer_seed_corpus.zip fuzz/corpus/*
+cp $OUT/zip_entry_read_fuzzer_seed_corpus.zip $OUT/zip_stream_copy_fuzzer_seed_corpus.zip
