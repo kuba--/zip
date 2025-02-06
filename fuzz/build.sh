03b9e267504d66e6687287bb20195aa18fe99286
kuba-- <kuba--@users.noreply.github.com>
2025-02-06 14:01:08 +0100

    Remove fuzz stuff

--- fuzz/build.sh
+++
@@ -1,8 +0,0 @@
-cd $SRC/zip
-
-mkdir -p build
-cmake -S . -B build -DCMAKE_C_COMPILER_WORKS=1 -DZIP_BUILD_FUZZ=ON && cmake --build build --target install
-
-# Prepare corpora
-zip -q $OUT/zip_entry_read_fuzzer_seed_corpus.zip fuzz/corpus/*
-cp $OUT/zip_entry_read_fuzzer_seed_corpus.zip $OUT/zip_stream_copy_fuzzer_seed_corpus.zip
