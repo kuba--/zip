649a16c5ea701c91c0b013ff614f7259012170b2
Kuba Podgórski <4056521+kuba--@users.noreply.github.com>
2025-06-27 14:34:43 +0200

    Add test_data (#377)

--- fuzz/read_entry_fuzzer.c
+++ fuzz/read_entry_fuzzer.c
@@ -2,37 +2,32 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-int LLVMFuzzerTestOneInput(const uint8_t *data, const size_t size)
-{
-    void *buf = NULL;
-    size_t bufsize = 0;
+int LLVMFuzzerTestOneInput(const uint8_t *data, const size_t size) {
+  void *buf = NULL;
+  size_t bufsize = 0;
 
-    struct zip_t *zip = zip_stream_open((const char *)data, size, 0, 'r');
-    if (NULL == zip)
-    {
-        goto end;
-    }
+  struct zip_t *zip = zip_stream_open((const char *)data, size, 0, 'r');
+  if (NULL == zip) {
+    goto end;
+  }
 
-    const ssize_t zip_entries_count = zip_entries_total(zip);
+  const ssize_t zip_entries_count = zip_entries_total(zip);
 
-    if (zip_entries_count <= 0)
-    {
-        goto end;
-    }
+  if (zip_entries_count <= 0) {
+    goto end;
+  }
 
-    if (0 != zip_entry_openbyindex(zip, 0))
-    {
-        goto end;
-    }
+  if (0 != zip_entry_openbyindex(zip, 0)) {
+    goto end;
+  }
 
-    zip_entry_read(zip, &buf, &bufsize);
+  zip_entry_read(zip, &buf, &bufsize);
 
 end:
-    zip_entry_close(zip);
-    if (NULL != zip)
-    {
-        zip_close(zip);
-    }
-    free(buf);
-    return 0;
+  zip_entry_close(zip);
+  if (NULL != zip) {
+    zip_close(zip);
+  }
+  free(buf);
+  return 0;
 }
