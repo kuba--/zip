2bb9ec249442f79d987c4595a82530e9dfd6fe46
kuba-- <kuba--@users.noreply.github.com>
2025-06-28 14:13:39 +0200

    Refine and simplify fuzzing

--- fuzz/fuzz_stream.c
+++ fuzz/fuzz_stream.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, const size_t size) {
-  /* Discard inputs larger than 1Mb. */
+  /* Discard inputs larger than 1MB. */
   static const size_t MaxSize = 1024 * 1024;
   if (size < 1 || size > MaxSize) {
     return 0;
@@ -11,7 +11,8 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, const size_t size) {
 
   char *outbuf = NULL;
   size_t outbufsize = 0;
-  struct zip_t *zip = zip_stream_open(NULL, 0, ZIP_DEFAULT_COMPRESSION_LEVEL, 'w');
+  struct zip_t *zip =
+      zip_stream_open(NULL, 0, ZIP_DEFAULT_COMPRESSION_LEVEL, 'w');
 
   zip_entry_open(zip, "test");
   zip_entry_write(zip, data, size);
